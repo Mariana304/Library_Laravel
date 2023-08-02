df8d2f07ac283b3265442fa331b8a5945aa83c27
Mariana304 <marianaortegaortiz92@gmail.com>
2023-08-02 15:25:31 -0500

    Avance de diseño

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -7,52 +7,110 @@
 @section('contect')
 
         <h1>Actualizar tarea</h1>
+        <div class="container bg-slate-100 shadow-lg  rounded-lg   w-96 sm:w-3/4 xl:w-2/4 2xl:w-2/5 flex flex-col text-center mx-auto  mt-10">
 
 
-        <form action=" {{ route('books.update', $book , $author) }}" method="POST" enctype="multipart/form-data" >
-
-            @csrf
-            @method('patch')
-
-
-            <label for="title">Libro</label><br>
-
-
-            <input type="text" name="title" value="{{ old('title', $book->title) }}"><br><br>
-
-
-            <label for="summary">Descripción</label><br>
-
-            <textarea name="summary">{{ old('summary', $book->summary) }}</textarea><br><br>
+            <div class="mt-12 uppercase text-2xl italic font-semibold">
+                <h1>Actualizar libro</h1>
             </div>
+    
+            <form action=" {{ route('books.update', $book ) }}" method="POST" enctype="multipart/form-data" >
+    
+                @csrf
 
-
-
-            <select name="author_id">
-
-                @foreach ($author as $author)
-                    <option value="{{ $author->id }}">{{ old('author', $author->name) }}</option>
-                @endforeach
-
-            </select><br><br>
-
-
-
-            <label for="pages">Numero de paginas</label>
-            <input type="number" name="pages" value="{{ old('pages', $book->pages) }}"><br><br>
-
-            <label for="price">Precio</label>
-            <input type="number" name="price" value="{{ old('price', $book->price) }}"><br><br>
-
+                @method('patch')
+    
+    
+                <div class="text-center font-bo mx-auto w-auto text-xl  mb-9 mt-16 ">
+    
+                    <div>
+                        <label for="title">Titulo</label><br>
+                    </div>
+    
+                    <input type="text" class="rounded-md border w-96 " name="title" value="{{ old('title', $book->title) }}">
+                </div>
+    
+    
+                <div class=" text-center mx-auto w-auto text-xl  mb-5  ">
+    
+                    <div class="mb-3"><label for="">Descripción</label></div>
+    
+                    <textarea name="summary" class="border rounded-md w-96 h-36">{{ old('summary', $book->summary) }}</textarea>
+                </div>
+    
+    
+    
+                <div class="px-6 py-3 rounded-lg ">
+                    <label for="author_id" class="text-xl">Autor</label>
+    
+                    <select name="author_id">
+    
+                        @foreach ($author as $author)
+                            <option value="{{ $author->id }}">{{ $author->name }}</option>
+                        @endforeach
+    
+                    </select><br><br>
+                </div>
+    
+    
+    
+                <div class="text-center font-bo mx-auto w-auto text-xl  mb-9 mt-16 ">
+    
+                    <div>
+                        <label for="pages">Numero de paginas</label><br>
+                    </div>
+    
+                    <input type="text" class="rounded-md border w-96 " name="pages" value="{{ old('pages', $book->pages) }}">
+                </div>
+    
+    
+                <div class="text-center font-bo mx-auto w-auto text-xl  mb-9 mt-16 ">
+    
+                    <div>
+                        <label for="price">Precio</label><br>
+                    </div>
+    
+                    <input class="rounded-md border w-96 " type="number" name="price" value="{{ old('price', $book->price) }}">
+                </div>
+    
+    
+    
+                <div class="text-center font-bo mx-auto w-auto text-xl  mb-9 mt-16 ">
+    
+                    <input type="radio" name="format" value="{{ old('format') }} 1" checked>
+                    <label for="2">Impreso</label>
+                </div>
+    
+                <div>
+                    <input type="radio" name="format" value="2">
+                    <label for="format">E book</label>
+                </div><br><br>
+       
+    
+    
+    
+        <div class="text-center font-bo mx-auto w-auto text-xl  mb-9 mt-16 ">
+    
             <label for="path_cover">Portada del libro</label>
-            <input type="file" name="path_cover" value="{{ old('path_cover' , $book->path_cover) }}"><br><br>
-
-
-
-            <button type="submit">Actualizar</button>
-
+            <input type="file" name="path_cover" value="{{ old('path_cover') }}"><br><br>
+    
+        </div>
+    
+    
+    
+        <div class="px-6 py-3 rounded-lg  mb-7">
+    
+            <button class="border w-32 rounded-lg h-11 mx-4 bg-lime-600 text-white" type="submit">Agregar
+                libro</button>
+        </div>
+    
 
         </form>
+    
+    </div>
+
+
+
 
 
 
--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,11 @@
 
 @section('title', 'libros')
 
-
+    <style>
+        .header{
+            background-image: url({{ asset('storage/images/bg-library.png') }})
+        }
+    </style>
 
 
 
@@ -19,17 +23,25 @@
 
     <div class="container mx-auto mt-5 text-center rounded-lg  ">
 
-        <header class="header bg-fixed h-72 sm:h-80  rounded-lg ">
+        <header class="header bg-fixed h-72 sm:h-80  rounded-lg border ">
 
-
-            <p class=" p-28 sm:p-40  text-3xl sm:text-5xl italic font-bold text-center">Biblioteca</p>
+          
+            <p class=" p-28 sm:p-40 text-white text-3xl sm:text-5xl italic font-bold text-center">Biblioteca</p>
 
         </header>
 
-        <h1 class=" font-bold underline">Bienvenidos a la biblioteca</h1>
+        <div class="grid justify-items-end py-2 mt-10  ">
+            <div class=" mb-4 rounded-lg bg-lime-600 h-10  text-xl text-white  ">
 
+                <a href="{{ route('books.create') }}">
 
-        <a href="{{ route('books.create') }}">Nuevo libro</a><br><br><br>
+                    <p class=" flex flex-col italic  h-9 py-1 rounded-lg text-center mx-2 w-28 ">Nuevo libro</p>
+
+                </a>
+
+            </div>
+            
+        </div>
 
         <div class=" border  grid lg:grid-cols-2  2xl:grid-cols-3  bg-slate-100  mt-4 sm:px-1 sm:py-1  rounded-lg ">
 
@@ -37,22 +49,22 @@
 
             @foreach ($book as $book)
                 <div
-                    class="flex flex-col shadow-lg rounded-lg bg-white w-4/5 h-80 sm:h-80 md:h-60 mx-auto mb-12 mt-8 md:max-w-lg md:flex-row ">
+                    class="flex flex-col overflow-hidden shadow-lg text-overflow rounded-lg bg-white w-4/5 h-80 sm:h-80 md:h-60 mx-auto mb-12 mt-8 md:max-w-lg md:flex-row ">
                     <img class="h-48 w-full rounded-t-lg object-cover sm:h-48  md:w-48 md:h-auto md:rounded-none md:rounded-l-lg"
                         src="/storage/{{ $book->path_cover }}">
 
 
                     <div class="flex flex-col sm:h-80 justify-start p-6">
 
-                        <h5 class="mb-2 text-lg   font-medium text-neutral-800">
+                        <h5 class="mb-2 text-sm sm:text-lg font-medium text-neutral-800">
 
                             <a href="{{ route('books.show', $book) }}"> * {{ $book->title }} </a>
 
                         </h5>
 
-                        <p>
+                        <p class="">
 
-                            {{ Str::limit($book->summary, 30) }}
+                            {{ Str::limit($book->summary, 50) }}
                         </p>
 
                         <p>
